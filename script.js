ff2e5a5cc93004c9d0a3887ab3d82c699e8f4bb3
Joshua <laikabeatsdenver@gmail.com>
2020-09-30 21:08:59 -0600

    high score functions, local storage, and updated HTML

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//add vars to reveal the containers within the html doc
-var startButton = document.getElementById("start-btn"); // start-button
-var questionContainerElement = document.getElementById("quizContainer"); // quiz container button 
+//HTML variables
+var startButton = document.getElementById("start-btn");
+var questionContainerElement = document.getElementById("quizContainer");
 var questionElement = document.getElementById("question");
 var anwserButtonsElement = document.getElementById("anwser");
 var choiceA= document.getElementById("A");
@@ -10,15 +10,15 @@ var choiceD= document.getElementById("D");
 var scoreContainer= document.getElementById("score-container");
 var initials = document.getElementById("initials");
 var lastScore = document.getElementById("lastScore");
+//
 
 
-
-//timer vars 
+//timer variables 
 var timeElement= document.getElementById("timer");
 var secondsLeft = 100;
+//
 
-
-//timer start functiom
+//Timer count down function
 var myCountDown = setInterval(function(){
     secondsLeft--;
     timeElement.textContent= secondsLeft + "seconds left until the end";
@@ -27,74 +27,75 @@ var myCountDown = setInterval(function(){
         clearInterval(myCountDown)
     }
 }, 1000)//milliseceonds 
+//
 
-//questions array
+//Questions,answers, and correct choice array
 var questions = [
 
     {
 
-        question : "What does HTML stand for?",
+        question : "What does the HTML DOM method getElementById() do?",
 
         
-        choiceA : "A",
+        choiceA : "Returns the element that has the ID attribute with the specified value.",
 
-        choiceB : "Wrong",
+        choiceB : "Returns the first element that matches a specified CSS selector(s) in the document.",
 
-        choiceC : "Wrong",
+        choiceC : "Returns a collection of all elements in the document with the specified class name.",
 
         correct : "A"
 
     },{
 
-        question : "What does CSS stand for?",
+        question : "What does the HTML DOM method getElementsByClassName() do?",
 
         
+        choiceA : "Returns the first element that matches a specified CSS selector(s) in the document.",
 
-        choiceA : "Wrong",
+        choiceB : "Returns a collection of all elements in the document with the specified class name.",
 
-        choiceB : "Correct",
-
-        choiceC : "Wrong",
+        choiceC : "Returns the element that has the ID attribute with the specified value.",
 
         correct : "B"
 
     },{
 
-        question : "What does JS stand for?",
+        question : "What does HTML DOM method querySelector() do?",
 
         
 
-        choiceA : "Wrong",
+        choiceA : "Returns a collection of all elements in the document with the specified class name.",
 
-        choiceB : "Wrong",
+        choiceB : "Returns the element that has the ID attribute with the specified value.",
 
-        choiceC : "Correct",
+        choiceC : "Returns the first element that matches a specified CSS selector(s) in the document.",
 
         correct : "C"
 
     }
 
 ];
-//var 
+//
+
+//Variables to cycle through question prompt and answer buttons 
 var lastQuestion = questions.length -1;
 var runningQuestion = 0;
+//
 
-
-//added event listener so that when the start button is clicked it will start the quiz
+//Gives start button a click function
 startButton.addEventListener("click", startQuiz,)
+//
 
-//function to hide start button, reveal the first question & start the quiz 
+//This function hides the start button, brings up the first question/answers, and starts the timer.
 function startQuiz (){
-    console.log ("test");
-    startButton.classList.add("hide");
-    questionContainerElement.classList.remove("hide");
-    timeElement.classList.remove ("hide");
+    startButton.classList.add("hide"); //hides start button
+    questionContainerElement.classList.remove("hide"); //reveals question container
+    timeElement.classList.remove ("hide"); //starts timer countdown
     renderQuestion();
-    
     }
-    
+//   
 
- //functinon to show questions & choices    
+ //Shows question and answer buttons in question container 
 function renderQuestion (){
    var q = questions[runningQuestion];
 
@@ -102,49 +103,51 @@ function renderQuestion (){
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
-    
-
 } 
+//
 
+//Gives answer buttons click function
 anwserButtonsElement.addEventListener("click", checkAnwser)
-    console.log ("hello ");
+    //console.log ("hello "); this was to test the answer buttons
 
 
-
+//Checks to see if the answer clicked is correct, deducts 30 seconds if a wrong answer is clicked, and cycles to the next question. Once the last question is answered the timer is stopped.
     function checkAnwser (e){
         e.preventDefault();
         console.log (e.target)
-        var userAnswer = e.target.getAttribute("id"); //// A, B, C, or D
+        var userAnswer = e.target.getAttribute("id"); ////Checks to see if A, B, or C is correct
             if (userAnswer === questions[runningQuestion].correct){
                 console.log ("yes");
-        
             } else {
-                console.log ("no");
-                secondsLeft -= 30;
+                secondsLeft -= 30; //subtracts 30 seconds if the user clicks the wrong answer
             }
             if (runningQuestion < lastQuestion){
-                renderQuestion (runningQuestion++);
+                renderQuestion (runningQuestion++); //prompts next question until there is none left
             } else {
-                clearInterval(myCountDown);
+                console.log ("finish")
+                clearInterval(myCountDown); //Stops timer
                 showScore();
-
-            }
-            
+            }        
     }
+//
+
+//hides question container, reveals high score/save container, and lists the last high score.
 function showScore(){
     scoreContainer.classList.remove("hide");
     startButton.classList.remove("hide");
     questionContainerElement.classList.add("hide");
-    lastScore.classList.remove("hide");
+    scoreContainer.classList.remove("hide");
 }
+//
 
+//HTML variables used to store and retrieve score
 var initialsInput = document.querySelector("#initials");
 var scoreInput = document.querySelector("#finalScore");
 var saveScore = document.querySelector("#saveScore");
 var msgDiv = document.querySelector("#msg");
 var userInitials = document.querySelector("#user-initials");
 var userScore = document.querySelector("#user-score");
-
+//
 
 renderLastRegistered();
 
@@ -153,9 +156,11 @@ function displayMessage(type, message) {
   msgDiv.setAttribute("class", type);
 }
 
+//Retrieves and displays last high score
 function renderLastRegistered() {
   var initials = localStorage.getItem("initials");
   var finalScore= localStorage.getItem("finalScore");
+
 
   if (!initials || !finalScore) {
     return;
@@ -164,7 +169,9 @@ function renderLastRegistered() {
   userInitials.textContent = initials;
   userScore.textContent = finalScore;
 }
+//
 
+//Saves high score and intials entered by user
 saveScore.addEventListener("click", function(event) {
   event.preventDefault();
 
@@ -183,96 +190,4 @@ saveScore.addEventListener("click", function(event) {
     renderLastRegistered();
   }
 });
-
-//create a custom atribute to show if that is correct or not 
-
-//apend to page 
-
-
-
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (var i=0; i <questions.length; i++);{
-//     questionForm.display (questions[i].promt);
-//     if (response==)
-// }
-
-
-
-
-// startButton.addEventListener("click", generateQuiz)
-
-
-// function generateQuiz (){
-//     event.preventDefault();
-  
-
-//     }
-
-// }
-
-
-
-//varables
-
-
-//when the user clicks the start button the first question will appear & the timer will start 
-
-    //the timer will start with 60 seconds on the clock and count down
-
-    //the questions will be stored as a object within an  array 
-
-    
-        //Questions:
-            //1. How do you comment in a Java Script File 
-                //a. <!--  
-                //b. //
-                //c. !!~
-                // correct button/anwser=b
-            //2. What is the proper way to save a Java Script File?
-                //a. script.js  
-                //b. script.javasript
-                //c. java.script
-                // correct button/anwser=a
-
-
-//if the user clicks the correct button the user will go to the next question
-
-
-//if the user doesnt select the correct button, the user will loose 10 seconds from the clock and go to the next question
-
-//this function will loop until the quiz is complete or the user runs out of time 
-
-//the user will see their score & add their initails & see the history of high scores 
-
-//the user will be promted to retry the game 
-
-
-//call funtctions
-
-//event listenrs
-
-
-
-// for (var i=0;i<numQuestions;i++){
-//     if (userInput[i]==answers[i]){
-//     score += 1;
-//     }
-//     else{
-//     score += 0;
-//     }
-    
+//
